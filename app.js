b524d4a5c83f503857429d1febff2cd1f7406a31
onamoiloa <onalennatumelo2@gmail.com>
2025-02-08 12:46:01 +0200

    Added search functionality using the API

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 function refreshWeather(response) {
-let temperatureElement = document.querySelector("#temperature");
-temperatureElement = response.data.temperature.current;
-let cityElement = document.querySelector("#city");
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+        let cityElement = document.querySelector("#city");
 
-cityElement.innerHTML = response.data.city;
-temperatureElement.innerHTML = Math.round(temperature);
-
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
-    let apiKey = "dc010eb1e73f4228b30225823252501";
-    let apiUrl = 'http://api.weatherapi.com/v1/current.json?key=${dc010eb1e73f4228b30225823252501}&q=&{cities}&aqi=no';
-     axios.get(apiUrl).then(refreshWeather);
+    let apiKey = "bb97c23fo71t6ba9f57527004105a77b";
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=bb97c23fo71t6ba9f57527004105a77b&units=metric';
+    axios.get(apiUrl).then(refreshWeather);
 }
 
-
-function handleSearchSubmit(event)  {
+function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-   
-   searchCity(searchInput.value);
-
+    
+    searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+let searchFormElement =  document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit); 
 
-searchCity("Paris");
+searchCity("Paris"); 
