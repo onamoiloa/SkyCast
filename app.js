4ba0d76b17545a181100b6d3026ab1ee27ae5737
onamoiloa <onalennatumelo2@gmail.com>
2025-02-08 22:54:32 +0200

    Injected more weather data

--- app.js
+++ app.js
@@ -2,14 +2,44 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
         let cityElement = document.querySelector("#city");
+        let descriptionElement = document.querySelector("#description");
+        let humidityElement = document.querySelector("#humidity");
+        let windSpeedElenent = document.querySelector("#wind-speed");
+        let timeElement = document.querySelector("#time");
+        let date = new Date(response.data.time * 1000);
 
-    cityElement.innerHTML = response.data.city;
+console.log(response.data);
+
+     cityElement.innerHTML = response.data.city;
+     
+     timeElement.innerHTML = formatDate(date);
+        descriptionElement.innerHTML= response.data.condition.description;
+        humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+        windSpeedElenent.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
+}
+ 
+function formatDate(date) {
+    let day = date.getDay();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let days = [
+            "Monday", 
+            "Tuesday",
+            "Wednesday",
+             "Thursday",
+              "Friday", 
+              "Saturday", 
+              "Sunday"
+            ];
+    let date = days[date.getDay()];
+
+        return`${day} ${hours}:${minutes}';
 }
 
 function searchCity(city) {
     let apiKey = "bb97c23fo71t6ba9f57527004105a77b";
-    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=bb97c23fo71t6ba9f57527004105a77b&units=metric';
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=bb97c23fo71t6ba9f57527004105a77b&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
 }
 
