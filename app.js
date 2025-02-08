51ddbc1faaf75130967d8963eb72f927310b36d1
onamoiloa <onalennatumelo2@gmail.com>
2025-02-08 23:37:33 +0200

    Added weather icons

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ function refreshWeather(response) {
         let windSpeedElenent = document.querySelector("#wind-speed");
         let timeElement = document.querySelector("#time");
         let date = new Date(response.data.time * 1000);
+        let iconElement = document.querySelector("#icon");
 
-console.log(response.data);
+         icon.innerHTML =`<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 
      cityElement.innerHTML = response.data.city;
-     
      timeElement.innerHTML = formatDate(date);
         descriptionElement.innerHTML= response.data.condition.description;
         humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
@@ -20,7 +20,6 @@ console.log(response.data);
 }
  
 function formatDate(date) {
-    let day = date.getDay();
         let hours = date.getHours();
         let minutes = date.getMinutes();
         let days = [
@@ -32,14 +31,17 @@ function formatDate(date) {
               "Saturday", 
               "Sunday"
             ];
-    let date = days[date.getDay()];
+    let day = days[date.getDay()];
 
-        return`${day} ${hours}:${minutes}';
+    if (minutes < 10){
+        minutes=`0${minutes}`
+    }
+        return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
     let apiKey = "bb97c23fo71t6ba9f57527004105a77b";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=bb97c23fo71t6ba9f57527004105a77b&units=metric`;
+    let apiUrl = ` https://api.shecodes.io/weather/v1/current?query=${city}&key=bb97c23fo71t6ba9f57527004105a77b&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -52,5 +54,4 @@ function handleSearchSubmit(event) {
 
 let searchFormElement =  document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit); 
-
 searchCity("Paris"); 
