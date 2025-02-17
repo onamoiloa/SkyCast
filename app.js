f061369fe8dbbab6d6c8fa5ec1c9df5348e37d54
onamoiloa <onalennatumelo2@gmail.com>
2025-02-18 00:31:37 +0200

    injected the forecast HTML from JavaScript

--- app.js
+++ app.js
@@ -52,6 +52,32 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function displayForecast(){
+    let forecastElement = document.querySelector("#forecast");
+
+let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+let forecastHtml = "";
+
+days.forEach(function (day){
+forecastHtml+= `
+<div class="weather-forecast-day">
+<div class="weather-forecast-date">${day}</div>
+<div class="weather-forecast-icon">⛅</div>
+<div class="weather-forecast-temperatures">
+<div class="weather-forecast-temperature">
+<strong>15&deg;</strong>
+</div>
+<div class="weather-forecast-temperature">9&deg;</div>
+</div>
+</div>
+`;
+});
+
+forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement =  document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit); 
+
 searchCity("Paris"); 
+displayForecast();
